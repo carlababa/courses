7feedcbadba5f9125105345c042db4ac1e303cdc
Carla Babadopulos <cacababa@gmail.com>
2017-01-28 17:42:36 +0100

    Initialize project with react bootstrap and material lite

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'material-design-lite/material.min.js';
+import './index.css';
+import './material.min.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
